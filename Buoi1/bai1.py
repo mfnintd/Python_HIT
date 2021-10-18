3220135d3d8bbd70c147d19030dab4d1f2166f24
= <=>
2021-10-18 09:39:15 +0700

    complete

--- Buoi1/bai1.py
+++ Buoi1/bai1.py
@@ -3,3 +3,5 @@ import math
 R = float(input())
 
 print("S =", math.pi * R * R)
+
+print("C =", 2 * math.pi * R)
---
+++ Buoi1/bai2.py
@@ -0,0 +1,5 @@
+import math
+
+x = float(input())
+
+print((x * x + math.e ** x + math.sin(x)) / math.sqrt(x * x + 1))
