b5867a416a45822aa004dd2728924f5ceb9cb1af
= <=>
2021-11-01 11:14:40 +0700

    done

--- Buoi3/Bai2.py
+++ Buoi3/Bai2.py
@@ -1,4 +1,5 @@
 # Note: update sau khi đề bài clear hơn
+# Update
 
 tapHopDaCho = {1, 2, 3}
 tapHopKhac = {3, 4, 5}
--- Buoi3/Bai3.py
+++ Buoi3/Bai3.py
@@ -2,3 +2,5 @@ tapHopDaCho = {1, 2, 3}
 tapHopKhac = {3, 4, 5, 1, 2}
 
 print(tapHopDaCho == tapHopDaCho.intersection(tapHopKhac))
+
+# Update
